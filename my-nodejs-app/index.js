949b6c21038eb2f0367c02de2887fe340594ccc9
Ho Phuoc Hoan <phuochoan17032004@gmail.com>
2025-10-19 17:41:18 +0700

    update index.js

--- my-nodejs-app/index.js
+++ my-nodejs-app/index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
   res.json({
-    message: 'Hello from Helios v10.0!',
+    message: 'Hello from Helios v11.0!',
     version: process.env.VERSION || 'v1.0.0',
     timestamp: new Date().toISOString()
   });
